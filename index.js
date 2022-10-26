98ddd194ef2e5d470e350220eb780f1f642ab3ad
AnjanaBiju <anjanabiju93@gmail.com>
2022-10-27 04:50:15 +0530

    put and delete requests completed

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const app=express();
 const port=8081;
 
 app.use(express.json());
+app.use(express.urlencoded());
 //To get all users details
 app.get('/users',(req,res)=>{
+    
     res.status(200).json({
         success:true,
         data:users,
@@ -16,6 +18,7 @@ app.get('/users',(req,res)=>{
 });
 //For wrong urls or path
 app.get('*',(req,res)=>{
+    
     res.status(200).send({
         message:'This page is not exist',
     });
@@ -58,6 +61,56 @@ app.post('/users_signup',(req,res)=>{
         message:'User already exist',
     });
 });
+app.delete('/users_delete/:id',(req,res)=>{
+    const {id}=req.params;
+    console.log(id);//unwanted
+    user_id=Number(id)//unwanted
+    const user=users.find((each)=>each.id===user_id);
+    users.find((each)=>{//unwanted
+        console.log(each.id,"<====")//unwanted
+        console.log(Number(id),"<====")//unwanted
+    })//unwanted
+    console.log("===>",user);//unwanted
+    if(!user){
+        return res.status(404).json({
+            success:false,
+            message:'User not found',
+        });
+    }
+    index=users.indexOf(user);
+    users.splice(index,1);
+    return res.status(200).json({
+        message:"deleted successfully",
+    });
+})
+app.put('/user_update/:id',(req,res)=>{
+    const {id} = req.params;
+    const {data} = req.body;
+    console.log(req.body,'<====')
+    const user= users.find((each)=>each.id===Number(id));
+    console.log("==>")
+    if(!user)
+    return res.status(404).json({
+        success:false,
+        message:"User Not Found",
+    });
+    
+    const updateuser=users.map((each)=>{
+        if(each.id===Number(id)){
+            console.log("==> executed")
+            return{
+                ...each,
+                ...data,
+            }
+            return each;
+        }
+    });
+    return res.status(200).json({
+        success:true,
+        data:updateuser,
+    });
+});
+
 app.listen(port,()=>{
     console.log('server started')
 });
